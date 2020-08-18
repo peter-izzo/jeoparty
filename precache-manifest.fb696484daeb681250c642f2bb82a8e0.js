abfeab5c7b7213bc4bcc77ae2b99146998ab19d7
Peter Izzo <peter.izzo528@gmail.com>
2020-08-18 00:37:06 -0400

    Updates

---
+++ precache-manifest.fb696484daeb681250c642f2bb82a8e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9be79bdd5f1b3a72c1785ec912ef5cef",
+    "url": "/jeoparty/index.html"
+  },
+  {
+    "revision": "36fc0f645f24d3ce965f",
+    "url": "/jeoparty/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e148b711d1246cd95671",
+    "url": "/jeoparty/static/css/main.c1b68019.chunk.css"
+  },
+  {
+    "revision": "36fc0f645f24d3ce965f",
+    "url": "/jeoparty/static/js/2.13838b3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jeoparty/static/js/2.13838b3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e148b711d1246cd95671",
+    "url": "/jeoparty/static/js/main.661d7c4f.chunk.js"
+  },
+  {
+    "revision": "afeb36b436bfb7b7bd12",
+    "url": "/jeoparty/static/js/runtime-main.ee20406c.js"
+  },
+  {
+    "revision": "b0815c7805e78dc7f0c4bbe340ab113b",
+    "url": "/jeoparty/static/media/party_trebek.b0815c78.jpg"
+  }
+]);
